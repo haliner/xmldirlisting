1bbd39614cae859094198bab2ffcbebca665df68
Stefan Haller <haliner@googlemail.com>
2011-02-25 13:02:23 +0100

    setup.py added: Use distutils for simple installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='xmldirlisting',
+    version='0.1',
+    description='XML Dirlisting',
+    author='Stefan Haller',
+    author_email='haliner@googlemail.com',
+    url='https://launchpad.net/xmldirlisting',
+    license = 'GPLv3',
+    scripts = ['xmldirlisting'],
+    data_files = [('share/xmldirlisting', ['dirlisting.css',
+                                           'dirlisting.dtd',
+                                           'dirlisting.js',
+                                           'dirlisting.xslt'])]
+)
